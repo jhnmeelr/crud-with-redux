5a7700ba5a2259475c2ea05cf56cd7ae6be83e6b
jhnmeelr <jhn.meelr@gmail.com>
2016-12-08 13:32:15 +0200

    Server-side Validation and Record Creation (Part 2)

--- src/GameForm.js
+++ src/GameForm.js
@@ -1,6 +1,7 @@
 import React, { Component, PropTypes } from 'react';
 import classnames from 'classnames';
 import { connect } from 'react-redux';
+import { Redirect } from 'react-router';
 import { saveGame } from './actions';
 
 class GameForm extends Component {
@@ -12,7 +13,8 @@ class GameForm extends Component {
         title: '',
         cover: '',
         errors: '',
-        loading: false
+        loading: false,
+        done: false
     }
 
     handleChange = (e) => {
@@ -44,14 +46,14 @@ class GameForm extends Component {
             const { title, cover } = this.state;
             this.setState({ loading: true });
             this.props.saveGame({ title, cover }).then(
-                () => {},
+                () => { this.setState({ done: true }) },
                 (err) => err.response.json().then(({errors}) => this.setState({ errors, loading: false }))
             );
         }
     }
 
     render() {
-        return (
+        const form = (
             <form className={classnames('ui', 'form', { loading: this.state.loading })} onSubmit={this.handleSubmit}>
                 <h1>Add new game</h1>
 
@@ -82,6 +84,11 @@ class GameForm extends Component {
                     <button className="ui primary button">Save</button>
                 </div>
             </form>
+        );
+        return (
+            <div>
+                { this.state.done ? <Redirect to="/games" /> : form }
+            </div>
         );
     }
 }
